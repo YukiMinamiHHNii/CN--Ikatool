b8394eaeeaa189460c83e4540dc5118a16df2f71
YukiMinamiHHNii <homu.homu.nii@gmail.com>
2019-03-27 19:14:04 -0600

    Removes log

--- src/utils/APIUtils.js
+++ src/utils/APIUtils.js
@@ -4,7 +4,6 @@ export const getStagesData = data => {
 	return axios
 		.get(`${process.env.REACT_APP_MARINA_ENDPOINT}/data/stage`)
 		.then(response => {
-			console.log(response.data)
 			return response.data;
 		})
 		.catch(error => {
