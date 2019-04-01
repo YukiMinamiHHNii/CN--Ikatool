0dadb7c7aca0285f352c3d1c7c7d5d5b9e0ef698
YukiMinamiHHNii <homu.homu.nii@gmail.com>
2019-04-01 15:27:52 -0600

    Removes placeholder from image display on StageView component for the real deal

--- src/components/StageView.js
+++ src/components/StageView.js
@@ -5,9 +5,10 @@ export const StageView = props => {
 		props.stage && (
 			<article>
 				<h2>{props.stage.name}</h2>
-				<img className="col-sm-12"
-					src="https://firebasestorage.googleapis.com/v0/b/cn-marina-f3119.appspot.com/o/ability%2F71f6faa1d05ef4f1c1e449d217d18a5d?alt=media&token=1f232d4f-d727-4cd4-967e-7480c9176263"
-					alt="test"
+				<img
+					className="col-sm-12"
+					src={props.stage.thumbnail}
+					alt={props.stage.name}
 				/>
 			</article>
 		)
