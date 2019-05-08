3364672715cb7b798f69eed8077e9e380f14375e
YukiMinamiHHNii <homu.homu.nii@gmail.com>
2019-05-08 15:46:02 -0500

    Updates

---
+++ precache-manifest.9672868c5533cdf05fa3bafbb44a4e5d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e96c10743d49f56c93bacf80eb386706",
+    "url": "/CN--Pearl/static/media/Splatoon2.e96c1074.otf"
+  },
+  {
+    "revision": "cf96c398dc50855483d3ad29ee510df9",
+    "url": "/CN--Pearl/static/media/Splatoon.cf96c398.otf"
+  },
+  {
+    "revision": "0ac64d45f8eaa5b81694",
+    "url": "/CN--Pearl/static/js/runtime~main.83d72958.js"
+  },
+  {
+    "revision": "b305c181984be25d8b2c",
+    "url": "/CN--Pearl/static/js/main.b1c02249.chunk.js"
+  },
+  {
+    "revision": "102bb3f94be3e1d015e8",
+    "url": "/CN--Pearl/static/js/2.83af4e98.chunk.js"
+  },
+  {
+    "revision": "b305c181984be25d8b2c",
+    "url": "/CN--Pearl/static/css/main.2876f95c.chunk.css"
+  },
+  {
+    "revision": "102bb3f94be3e1d015e8",
+    "url": "/CN--Pearl/static/css/2.a6bf4ac2.chunk.css"
+  },
+  {
+    "revision": "8a6152dffe56182654f484ecbf99be4a",
+    "url": "/CN--Pearl/index.html"
+  }
+];
